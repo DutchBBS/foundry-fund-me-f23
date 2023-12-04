f1b67d60eaf40399a6a3c3ab86ad6b2fa2078baf
DutchBBS <support@builtbysoftware.com>
2023-12-04 20:48:09 +0100

    First commit

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+
+// 1. Depleoy Mocks when we are on a local anvil chain
+// 2. Keep track of contract address across different chains
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    // If we are on a local anvil chain, we will deploy mock contracts
+    // Otherwise, we will use the real contracts
+
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed; // Eth/USD pricefeed
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // Pricefeed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        // Pricefeed address
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        // Pricefeed address
+        // Deploy mock contracts
+        // Return mock contract addresses
+        vm.startBroadcast();
+        MockV3Aggregator mockAggregator = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockAggregator)
+        });
+
+        return anvilConfig;
+    }
+}
